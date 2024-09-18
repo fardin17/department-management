6833be2831513fb56de0100d8136b5178db26288
Topu-Roy <topuroy143333@gmail.com>
2024-09-18 12:31:17 +0600

    Fix: Deployment err

--- app/api/chapter/route.ts
+++ app/api/chapter/route.ts
@@ -3,23 +3,26 @@ import { type NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
-  const { ...chapterInfo, email } = data;
 
-  const userResponse = await fetch("http://localhost:4000/users");
-  const allUsers = (await userResponse.json()) as Database["users"];
-  const userId = allUsers.find((user) => user.email === email)?.id;
+  // ! Was giving Deployment error
 
-  const teacherResponse = await fetch("http://localhost:4000/teachers");
+  // const { ...chapterInfo, email } = data;
 
-  const allTeachers = (await teacherResponse.json()) as any;
+  // const userResponse = await fetch("http://localhost:4000/users");
+  // const allUsers = (await userResponse.json()) as Database["users"];
+  // const userId = allUsers.find((user) => user.email === email)?.id;
 
-  const teacherInfo = allTeachers.find((teacher) => teacher?.id === userId);
+  // const teacherResponse = await fetch("http://localhost:4000/teachers");
 
-  const updatedInfo = { ...teacherInfo, chapter: [...teacherInfo.chapter, chapterInfo] };
+  // const allTeachers = (await teacherResponse.json()) as any;
 
-  const response2 = await fetch("http://localhost:4000/teachers", {
-    method: "POST",
-    body: JSON.stringify(updatedInfo),
-    headers: { "Content-Type": "application/json" },
-  });
+  // const teacherInfo = allTeachers.find((teacher) => teacher?.id === userId);
+
+  // const updatedInfo = { ...teacherInfo, chapter: [...teacherInfo.chapter, chapterInfo] };
+
+  // const response2 = await fetch("http://localhost:4000/teachers", {
+  //   method: "POST",
+  //   body: JSON.stringify(updatedInfo),
+  //   headers: { "Content-Type": "application/json" },
+  // });
 }
